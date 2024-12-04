4337dc2dd9bde3d350bf575e89623dfc5c65256c
Khang Nguyen <nguyennhukhangvn@gmail.com>
2024-12-04 14:14:25 +0700

    Clean code usued

--- oauth-service/src/main/java/com/nnk/oauth/config/ServerWebSecurityConfig.java
+++ oauth-service/src/main/java/com/nnk/oauth/config/ServerWebSecurityConfig.java
@@ -13,10 +13,6 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 @Configuration
 @EnableWebSecurity
